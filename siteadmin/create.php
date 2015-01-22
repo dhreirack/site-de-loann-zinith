b255f4dd88c37590d1c2d677ca164bdcfe9c0c36
iseppiandrew@gmail.com <iseppiandrew@gmail.com@23955590-3938-c1aa-87cf-797e0c7e5bf6>
2015-01-22 13:39:54 +0000

    delete useless crap

--- siteadmin/create.php
+++
@@ -1,6 +0,0 @@
-<form action="index.php?action=store" method="post">
-	id : <input type="text" name="id">
-	titre : <input type="text" name="titre">
-	description : <input type="text" name="description">
-	<input type="submit">
-</form>
